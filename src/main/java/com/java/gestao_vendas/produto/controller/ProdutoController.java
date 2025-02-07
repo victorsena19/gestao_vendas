2a00fe537f309d5fe54ce8e82165de4c851b44cc
victorsena19 <victor_sena19@hotmail.com>
2025-02-07 15:32:38 -0400

    [change]: implementando anotacao para capturar os atributos do ProdutoDTO

--- src/main/java/com/java/gestao_vendas/produto/controller/ProdutoController.java
+++ src/main/java/com/java/gestao_vendas/produto/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.java.gestao_vendas.produto.dto.ProdutoDTO;
 import com.java.gestao_vendas.produto.entity.Produto;
 import com.java.gestao_vendas.produto.service.ProdutoService;
 import com.java.gestao_vendas.utils.Messege;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,20 +29,20 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public ResponseEntity<ProdutoDTO> criarProduto(ProdutoDTO produtoDTO){
-        ProdutoDTO novaProduto = produtoService.criarProduto(produtoDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novaProduto);
+    public ResponseEntity<ProdutoDTO> criarProduto(@Valid @RequestBody ProdutoDTO produtoDTO){
+        ProdutoDTO novoProduto = produtoService.criarProduto(produtoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoProduto);
     }
 
-    @PutMapping(value = {"{/id}"})
-    public ResponseEntity<ProdutoDTO> atualizaProduto(@PathVariable Long id, @RequestParam ProdutoDTO produtoDTO){
+    @PutMapping(value = {"/{id}"})
+    public ResponseEntity<ProdutoDTO> atualizaProduto(@Valid @PathVariable Long id, @RequestBody ProdutoDTO produtoDTO){
         ProdutoDTO produtoAtualizada = produtoService.atualizaProduto(id, produtoDTO);
         return ResponseEntity.ok().body(produtoAtualizada);
     }
 
     @DeleteMapping(value = {"/{id}"})
     public ResponseEntity<Messege> deleteProduto(@PathVariable Long id){
-        Messege produtoExcluida = produtoService.deletarProduto(id);
-        return ResponseEntity.ok().body(produtoExcluida);
+        Messege produtoExcluido = produtoService.deletarProduto(id);
+        return ResponseEntity.ok().body(produtoExcluido);
     }
 }
