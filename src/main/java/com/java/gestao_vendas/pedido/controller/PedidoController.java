0d728019eb8c9eeb4700f90af83f561c678c7ec3
victorsena19 <victor_sena19@hotmail.com>
2025-02-07 16:34:59 -0400

    [change]: implementando anotacao para capturar os atributos do pedidoDTO

--- src/main/java/com/java/gestao_vendas/pedido/controller/PedidoController.java
+++ src/main/java/com/java/gestao_vendas/pedido/controller/PedidoController.java
@@ -4,6 +4,7 @@ import com.java.gestao_vendas.pedido.dto.PedidoDTO;
 import com.java.gestao_vendas.pedido.entity.Pedido;
 import com.java.gestao_vendas.pedido.service.PedidoService;
 import com.java.gestao_vendas.utils.Messege;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,13 +30,13 @@ public class PedidoController {
     }
 
     @PostMapping
-    public ResponseEntity<PedidoDTO> criarPedido(@RequestBody PedidoDTO pedidoDTO){
+    public ResponseEntity<PedidoDTO> criarPedido(@Valid @RequestBody PedidoDTO pedidoDTO){
         PedidoDTO novaPedido = pedidoService.criarPedido(pedidoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaPedido);
     }
 
     @PutMapping(value = {"/{id}"})
-    public ResponseEntity<PedidoDTO> atualizaPedido(@PathVariable Long id, @RequestParam PedidoDTO pedidoDTO){
+    public ResponseEntity<PedidoDTO> atualizaPedido(@Valid @PathVariable Long id, @RequestParam PedidoDTO pedidoDTO){
         PedidoDTO pedidoAtualizada = pedidoService.atualizaPedido(id, pedidoDTO);
         return ResponseEntity.ok().body(pedidoAtualizada);
     }
