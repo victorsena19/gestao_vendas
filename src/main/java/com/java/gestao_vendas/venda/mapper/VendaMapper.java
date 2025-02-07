25f32beda3dcb37bbaf276493f2fd7e3597a5d7b
victorsena19 <victor_sena19@hotmail.com>
2025-02-07 15:43:24 -0400

    [fix]: correcao de nomenclatura na vendaMapper

--- src/main/java/com/java/gestao_vendas/venda/mapper/VendaMapper.java
+++ src/main/java/com/java/gestao_vendas/venda/mapper/VendaMapper.java
@@ -3,12 +3,14 @@ package com.java.gestao_vendas.venda.mapper;
 import com.java.gestao_vendas.venda.dto.VendaDTO;
 import com.java.gestao_vendas.venda.entity.Venda;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface VendaMapper {
-    VendaMapper INSTANCE = Mappers.getMapper(VendaMapper.class);
 
+    @Mapping(target = "itens.venda", ignore = true)
     VendaDTO toDTO(Venda venda);
+
+    @Mapping(target = "itens.venda", ignore = true)
     Venda toEntity(VendaDTO vendaDTO);
 }
