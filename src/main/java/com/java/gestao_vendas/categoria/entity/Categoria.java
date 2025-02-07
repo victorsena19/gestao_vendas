2ce106e31d17dfb6d9b89f73b3850b5dd5c7c9d6
victorsena19 <victor_sena19@hotmail.com>
2025-02-07 14:48:20 -0400

    [fix]: corrigindo relacionamento entre entidades

--- src/main/java/com/java/gestao_vendas/categoria/entity/Categoria.java
+++ src/main/java/com/java/gestao_vendas/categoria/entity/Categoria.java
@@ -1,6 +1,9 @@
 package com.java.gestao_vendas.categoria.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.java.gestao_vendas.empresa.entity.Empresa;
 import com.java.gestao_vendas.produto.entity.Produto;
 import jakarta.persistence.*;
@@ -8,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.List;
 
 @AllArgsConstructor
@@ -16,16 +18,16 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "categorias")
-public class Categoria implements Serializable {
+public class Categoria extends Persistence {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String nome;
 
-    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.MERGE, orphanRemoval = true)
+    @JsonBackReference
     private List<Produto> produto;
-
     private String descricao;
 
     @OneToOne(cascade = CascadeType.ALL)
